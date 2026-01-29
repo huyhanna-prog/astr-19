82c2c4245724dd09b0e7a7693ed9e284a1b54b0d
huyhanna-prog <taccat099@gmail.com>
2026-01-28 23:23:57 -0800

    Create j4.py

---
+++ j4.py
@@ -0,0 +1,8 @@
+class Cheetah:
+    def __init__(self):
+        self.arms_length = 29.0
+        self.legs_length = 29.0
+        self.number_of_eyes = 2
+        self.has_a_tail = True
+        self.is_furry = True
+
